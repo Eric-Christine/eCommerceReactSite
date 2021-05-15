88f82da9359a672126eba75123764fd37b911bad
Eric Chrsitine <ericjchristine@gmail.com>
2021-05-15 16:03:19 -0600

    added header, footer, and boostrap css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
 function App() {
   return (
     <>
+    <Header/>
+    <main>
       <h1>Welcome to ProShop</h1>
+    </main>
+    <Footer />
     </>
   );
 }
